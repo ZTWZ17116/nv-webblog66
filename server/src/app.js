8e3a000ca55ebb8d8ee560475dae2c6ee9702a7d
IZTWZI <joephandee@gmail.com>
2023-08-16 19:59:00 +0700

    page 82

--- server/src/app.js
+++ server/src/app.js
@@ -1,23 +1,29 @@
 let express = require('express')
 let bodyParser = require('body-parser')
+const { sequelize } = require('./models')
+
+const config = require('./config/config')
 
 const app = express()
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/status', function(req,res){
+app.get('/status', function (req, res) {
     res.send('Hello nodejs server belong to nattapon')
 })
 
-app.get('/hello/:name', function(req,res){
+app.get('/hello/:name', function (req, res) {
     console.log('hello - ' + req.params.name)
     res.send('sey hello with ' + req.params.name)
 })
 
 require('./route')(app)
 
-let port = 8081
-app.listen(port, function(){
-    console.log('server running on ' + port)
+let port = process.env.PORT || config.port
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(port, function () {
+        console.log('server running on ' + port)
+    })
 })
