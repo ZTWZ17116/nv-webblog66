10c7f9ae6d5855ad0e64b0c30be25230bf9e889d
IZTWZI <joephandee@gmail.com>
2023-08-16 18:51:16 +0700

    end 4

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,10 @@
 let express = require('express')
+let bodyParser = require('body-parser')
+
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get('/status', function(req,res){
     res.send('Hello nodejs server belong to nattapon')
@@ -8,6 +13,26 @@ app.get('/status', function(req,res){
 app.get('/hello/:name', function(req,res){
     console.log('hello - ' + req.params.name)
     res.send('sey hello with ' + req.params.name)
+})
+
+app.get('/user/:userID', function(req,res){
+    res.send('ดูข้อมูลผู้ใช้' + req.params.userID)
+})
+
+app.get('/users', function(req,res){
+    res.send('ดูข้อมูลผู้ใช้ทั้งหมด')
+})
+
+app.post('/user/', function(req,res){
+    res.send('สร้างผู้ใช้' + JSON.stringify(req.body))
+})
+
+app.put('/user/:userID', function(req,res){
+    res.send('แก้ไขข้อมูลผู้ใช้' + req.params.userID + JSON.stringify(req.body))
+})
+
+app.delete('/user/:userID', function(req,res){
+    res.send('ลบข้อมูลผู้ใช้' + req.params.userID + JSON.stringify(req.body))
 })
 
 let port = 8081
